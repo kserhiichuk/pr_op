b662afaeb55792ff1de8cc8391cb141ed2e609f1
kserhiichuk <127401317+kserhiichuk@users.noreply.github.com>
2023-06-08 22:30:23 +0300

    Create classes.h

---
+++ pr6-7/classes.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include<fstream>
+
+using namespace std;
+
+#define MAX 1000000
+#define MIN -1000000
+
+class File {
+    fstream file;
+    string filename;
+
+public:
+    File(string name);
+    void add(int);
+    int* read(int&);
+    void write_from_another(int, File*);
+    string get_filename();
+    void clear();
+    void is_open();
+};
