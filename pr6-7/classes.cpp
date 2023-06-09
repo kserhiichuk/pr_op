04607bb95e75921d6d834a8139248da490a90076
kserhiichuk <127401317+kserhiichuk@users.noreply.github.com>
2023-06-09 12:12:03 +0300

    Update classes.cpp

--- pr6-7/classes.cpp
+++ pr6-7/classes.cpp
@@ -28,31 +28,53 @@ int* File::read(int& size) {
     return numbers;
 }
 
-void File::write_from_another(int number_to_increase, File* fin) {
-    fin->file.open(fin->get_filename(), ios::binary | ios::in);
-    fin->is_open();
+void File::write_from_another(int number_to_increase, File* fin1, File* fin2) {
+    fin1->file.open(fin1->get_filename(), ios::binary | ios::in);
+    fin1->is_open();
+
+    fin2->file.open(fin2->get_filename(), ios::binary | ios::in);
+    fin2->is_open();
 
     file.open(filename, ios::binary | ios::out | ios::app);
     is_open();
 
-    int number;
-    while (fin->file.read(reinterpret_cast<char*>(&number), sizeof(int))) {
-            int result = number_to_increase * number;
-            if (result >= MAX) {
-                file.close();
-                fin->file.close();
-                throw "OVERFLOW!";
-            }
-            else if (result <= MIN) {
-                file.close();
-                fin->file.close();
-                throw "UNDERFLOW!";
-            }
+    int number1, number2;
+    bool found_common_number = false;
 
-            file.write(reinterpret_cast<char*>(&(result)), sizeof(int));
+    while (fin1->file.read(reinterpret_cast<char*>(&number1), sizeof(int))) {
+        while (fin2->file.read(reinterpret_cast<char*>(&number2), sizeof(int))) {
+            if (number1 == number2) {
+                found_common_number = true;
+                int result = number1 * number_to_increase;
+
+                if (result >= MAX) {
+                    fin1->file.close();
+                    fin2->file.close();
+                    file.close();
+                    throw "OVERFLOW!";
+                }
+                else if (result <= MIN) {
+                    fin1->file.close();
+                    fin2->file.close();
+                    file.close();
+                    throw "UNDERFLOW!";
+                }
+
+                file.write(reinterpret_cast<char*>(&result), sizeof(int));
+                break; 
+            }
+        }
+        fin2->file.clear();  // Reset the EOF flag for fin2
+        fin2->file.seekg(0, ios::beg);  
     }
+
+
+    fin1->file.close();
+    fin2->file.close();
     file.close();
-    fin->file.close();
+
+    if (!found_common_number) 
+        throw "No common numbers found!";
 }
 
 string File::get_filename() {
